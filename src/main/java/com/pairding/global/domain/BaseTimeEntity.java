15d5f770aa8cd5f1a4029ad8f3ccfebefd7221c4
Raon <cheol1998@kakao.com>
2025-12-18 08:06:48 +0000

    feat : 시간 엔티티 추가

---
+++ src/main/java/com/pairding/global/domain/BaseTimeEntity.java
@@ -0,0 +1,29 @@
+package com.pairding.global.domain;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+
+@Getter
+@MappedSuperclass
+public abstract class BaseTimeEntity {
+
+    @CreatedDate
+    @Column(name="created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name="updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+}
+
+
+
+
+
+
